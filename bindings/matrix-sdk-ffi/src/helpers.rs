215019f328f1caafb0a9118e265ebe93c7118b43
Jorge Martín <jorgem@element.io>
2024-10-28 12:30:57 +0100

    chore(ffi): Add `is_room_alias_valid` function
    This allows us to centralise the validation in the SDK instead of having separate checks per client.

--- bindings/matrix-sdk-ffi/src/helpers.rs
+++ bindings/matrix-sdk-ffi/src/helpers.rs
@@ -3,3 +3,8 @@ use std::sync::Arc;
 pub(crate) fn unwrap_or_clone_arc<T: Clone>(arc: Arc<T>) -> T {
     Arc::try_unwrap(arc).unwrap_or_else(|x| (*x).clone())
 }
+
+#[matrix_sdk_ffi_macros::export]
+pub fn is_room_alias_valid(room_alias: String) -> bool {
+    ruma_identifiers_validation::room_alias_id::validate(&room_alias).is_ok()
+}
